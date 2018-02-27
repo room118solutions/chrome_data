d4cb016f392e9e0cd9412427597d4ddd02d77317
Jim Ryan <jim@room118solutions.com>
2018-02-27 14:01:11 -0500

    Add Vehicle.find_by_style_id

--- lib/chrome_data/vehicle.rb
+++ lib/chrome_data/vehicle.rb
@@ -14,26 +14,51 @@ module ChromeData
           if vin_description = find_elements('vinDescription', response).first
             new.tap do |v|
               v.model_year = vin_description.attr('modelYear').to_i
-              v.division = vin_description.attr('division')
-              v.model = vin_description.attr('modelName')
-
-              v.styles = find_elements('style', response).map do |e|
-                Style.new(
-                  id: e.attr('id').to_i,
-                  name: e.attr('name'),
-                  trim: e.attr('trim'),
-                  name_without_trim: e.attr('nameWoTrim'),
-                  body_types: e.xpath("x:bodyType", 'x' => response.body.namespace.href).map do |bt|
-                    Style::BodyType.new(bt.attr('id').to_i, bt.text)
-                  end
-                )
-              end
-
-              v.engines = find_elements('engine', response).map do |e|
-                Engine.new(e.at_xpath("x:engineType", 'x' => response.body.namespace.href).text)
-              end
+              v.division   = vin_description.attr('division')
+              v.model      = vin_description.attr('modelName')
+              v.styles     = find_elements('style', response).map { |e| parse_style(e) }
+              v.engines    = parse_engines(response)
             end
           end
+        end
+      end
+
+      def find_by_style_id(style_id)
+        request 'styleId' => style_id do |response|
+          status = find_elements('responseStatus', response).first
+
+          if status.attr('responseCode') == 'Successful'
+            # We assume that only one style is returned here,
+            # since we requested a specific style ID,
+            # but the API docs aren't clear on that
+            style = find_elements('style', response).first
+
+            new.tap do |v|
+              v.model_year = style.attr('modelYear').to_i
+              v.division   = style.xpath("x:division", 'x' => response.body.namespace.href).first.text
+              v.model      = style.xpath("x:model", 'x' => response.body.namespace.href).first.text
+              v.styles     = [parse_style(style)]
+              v.engines    = parse_engines(response)
+            end
+          end
+        end
+      end
+
+      def parse_style(element)
+        Style.new(
+          id: element.attr('id').to_i,
+          name: element.attr('name'),
+          trim: element.attr('trim'),
+          name_without_trim: element.attr('nameWoTrim'),
+          body_types: element.xpath("x:bodyType", 'x' => element.namespace.href).map do |bt|
+            Style::BodyType.new(bt.attr('id').to_i, bt.text)
+          end
+        )
+      end
+
+      def parse_engines(response)
+        find_elements('engine', response).map do |e|
+          Engine.new(e.at_xpath("x:engineType", 'x' => response.body.namespace.href).text)
         end
       end
     end
--- test/chrome_data/vehicle_test.rb
+++ test/chrome_data/vehicle_test.rb
@@ -75,4 +75,69 @@ describe ChromeData::Vehicle do
       end
     end
   end
+
+  describe '.find_by_style_id' do
+    before do
+      ChromeData.configure do |c|
+        c.account_number = '123456'
+        c.account_secret = '1111111111111111'
+      end
+
+      VCR.use_cassette('wsdl') do
+        VCR.use_cassette('vehicles/387520') do
+          @vehicle = ChromeData::Vehicle.find_by_style_id('387520')
+        end
+      end
+    end
+
+    it 'returns a Vehicle object' do
+      @vehicle.must_be_instance_of ChromeData::Vehicle
+    end
+
+    it 'sets model year on Vehicle' do
+      @vehicle.model_year.must_equal 2017
+    end
+
+    it 'sets division on Vehicle' do
+      @vehicle.division.must_equal 'Acura'
+    end
+
+    it 'sets model on Vehicle' do
+      @vehicle.model.must_equal 'MDX'
+    end
+
+    describe 'styles' do
+      it 'sets id' do
+        @vehicle.styles.first.id.must_equal 387520
+      end
+
+      it 'sets name' do
+        @vehicle.styles.first.name.must_equal 'FWD w/Advance Pkg'
+      end
+
+      it 'sets trim' do
+        @vehicle.styles.first.trim.must_equal 'w/Advance Pkg'
+      end
+
+      it 'sets name_without_trim' do
+        @vehicle.styles.first.name_without_trim.must_equal 'FWD'
+      end
+
+      describe 'body_types' do
+        it 'sets name' do
+          @vehicle.styles.first.body_types.first.name.must_equal 'Sport Utility'
+        end
+
+        it 'sets id' do
+          @vehicle.styles.first.body_types.first.id.must_equal 13
+        end
+      end
+    end
+
+    describe 'engines' do
+      it 'sets type' do
+        @vehicle.engines.first.type.must_equal 'V6 Cylinder Engine'
+      end
+    end
+  end
 end
